4c16c53fc026b065d445eefaae3881fc84475b59
radush-v <vladymyr.r@radency.com>
2025-05-07 22:27:40 +0300

    feat: code refactoring

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replace-template');
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
-    if (pathName === '/' || pathName === '/overview') {
-        res.end('This is overview');
-    } else if (pathName === '/product') {
-        res.end('This is the PRODUCT');
-    } else if (pathName === '/api') {
+
+    const { query, pathname } = url.parse(req.url, true)
+
+    // Overview page
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        })
+
+        const cardsHTML = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+
+        res.end(output);
+    }
+
+    // Product page
+    else if (pathname === '/product') {
+        const product = dataObj[query.id];
+
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        })
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+    }
+
+    // API page
+    else if (pathname === '/api') {
         res.writeHead(200, {
             'content-type': 'application/json'
         })
         res.end(data);
     }
+
+    // Not found
     else {
         res.writeHead(404, {
             'content-type': 'text/html',
